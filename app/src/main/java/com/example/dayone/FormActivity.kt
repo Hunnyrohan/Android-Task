ed907e0294069502544aa613f1e400eb9138a941
Rohan <yadavroohan4545@gmail.com>
2024-11-19 23:02:52 +0545

    this is my task

---
+++ app/src/main/java/com/example/dayone/FormActivity.kt
@@ -0,0 +1,37 @@
+package com.example.dayone
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormActivity : AppCompatActivity() {
+    lateinit var EditText:EditText
+    lateinit var Button:Button
+    lateinit var TextView:TextView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form)
+        EditText = findViewById(R.id.editName)
+        Button = findViewById(R.id.submit)
+        TextView = findViewById(R.id.display)
+
+        Button.setOnClickListener {
+            val data : String = EditText.text.toString()
+            TextView.text = data
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dayone/RadioActivity.kt
@@ -0,0 +1,24 @@
+package com.example.dayone
+
+import android.os.Bundle
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RadioActivity : AppCompatActivity() {
+    lateinit var radioRadioButton:RadioButton
+    lateinit var radioPhoto:RadioButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_radio)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
